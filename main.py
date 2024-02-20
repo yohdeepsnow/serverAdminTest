63474ea0364751d8da4534472eab61647ad5959a
yohanmuliono <muliono.yohan@gmail.com>
2024-02-20 09:43:18 +0700

    menambahkan perkalian di branch baru

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def pengurangan(a,b):
 	total=a-b
 	return total
 
+def perkalian(a,b):
+	total=a*b
+	return total
 
 def main():
 	print(penambahan(10,10))
 	print(pengurangan(10,5))
+	print(perkalian(10,5))
 
 main()
